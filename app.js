f0278998e8dd32c6f8b192003c3d6198a1bf5c9e
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2025-08-06 21:00:04 -0700

    Add in github oauth path

--- app.js
+++ app.js
@@ -3,3 +3,41 @@ require('dotenv').config();
 const querystring = require('querystring')
 const app = express();
 
+async function getGithubUser (code) {
+  let accesstoken = ''
+  const token = await fetch(`https://github.com/login/oauth/access_token?client_id=${process.env.clientId}&client_secret=${process.env.gsecret}&code=${code}`, {method: 'POST'}
+  ).then(response => {
+    return response.text()
+  })
+  .then(response => {
+    const decoded = querystring.parse(response)
+    accesstoken = decoded.access_token
+  })
+  .catch(error => {
+    throw error
+  })
+  return await fetch('https://api.github.com/user', {
+    headers: {Authorization: `Bearer ${accesstoken}`}
+  })
+  .then(response => {
+    return response.json()
+  })
+}
+
+app.get('/api/auth/github', async (req, res)=>{
+  const code = req.query.code
+  const path = req.query.path
+
+  if (!code){
+    throw ('no code')
+  }
+  const user = await getGithubUser(code)
+  const userstring = JSON.stringify(user)
+  res.cookie('jwt', userstring, {
+    httpOnly: true,
+    domain: "localhost:5174" //maybe just loaclhost
+  })
+  res.redirect(`${process.env.host}` + path + '?' + userstring)
+})
+
+app.listen(4000, ()=>console.log('Chatroom - listening on port '+4000+'!'))
